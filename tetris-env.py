6795198a42d3151051ad43033d9aab991aee6d66
CaptainN3ro <nicolas-schwarz@outlook.de>
2022-04-19 19:04:13 +0200

    Added TetrisEnv Basic code

--- tetris-env.py
+++ tetris-env.py
@@ -1,6 +1,22 @@
 import gym
+import pygame
+from game import Game
 
 class TetrisEnv(gym.Env):
 
     def __init__(self,env_config={}):
+        self.game = Game()
+
+    def step(self, action):
+        self.game.step()
+        self.game.clock.tick(self.game.fps)
+        rect = pygame.Rect(25, 25, 100, 50)
+        sub = pygame.screen.subsurface(rect)
+        pygame.image.save(sub, "screenshot.jpg")
+
+    
+    def render(self):
+        pass
+
+    def reset(self):
         pass
