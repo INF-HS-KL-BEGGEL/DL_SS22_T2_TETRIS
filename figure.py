e5b55666f05e4fb1bbff04cc88f70264c435aba8
Julian Paulus <julian.paulus@hs-kl.de>
2022-04-24 11:19:35 +0200

    more reward for % of line filled?

--- figure.py
+++ figure.py
@@ -4,35 +4,43 @@ from shape import *
 from tetris_util import colors
 
 shapes: List[Shape] = [
-    LINE,
-    Z,
-    Z_INVERSE,
-    L_RIGHT,
-    L_LEFT,
-    T,
-    BLOCK,
+	LINE,
+	Z,
+	Z_INVERSE,
+	L_RIGHT,
+	L_LEFT,
+	T,
+	BLOCK,
 ]
 
 
 class Figure:
-    x = 0
-    y = 0
+	x = 0
+	y = 0
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.type = random.randint(0, len(shapes) - 1)
-        self.color = shapes[self.type].color()
-        self.rotation = 0
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.type = random.randint(0, len(shapes) - 1)
+		self.color = shapes[self.type].color()
+		self.rotation = 0
 
-    def image(self):
-        return shapes[self.type].rotation(self.rotation)
+	def image(self):
+		return shapes[self.type].rotation(self.rotation)
 
-    def rotate(self):
-        self.rotation = (self.rotation + 1) % shapes[self.type].rotation_count()
+	def rotate(self):
+		self.rotation = (self.rotation + 1) % shapes[self.type].rotation_count()
 
-    def width(self) -> int:
-        return shapes[self.type].width()
+	def width(self) -> int:
+		return shapes[self.type].width()
 
-    def height(self) -> int:
-        return shapes[self.type].height()
+	def height(self) -> int:
+		return shapes[self.type].height()
+
+	def __eq__(self, other):
+		if not isinstance(other, Figure):
+			# don't attempt to compare against unrelated types
+			return NotImplemented
+
+		return self.type == other.type and self.rotation == other.rotation \
+			   and self.x == other.x and self.y == other.y
--- tetris_env.py
+++ tetris_env.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 import gym
 import pygame
 from game import Game
@@ -11,13 +13,18 @@ class TetrisEnv(gym.Env):
 		self.last_score = 0
 
 	def step(self, action):
+
+		figure_before_step = self.game.tetris.figure
+		next_figure_before_step = self.game.tetris.next_figure
+		field_before_step = copy(self.game.tetris.field)
+
 		self.game.step(mode='ai', action=action)
 
 		observation = self.render()
 		done = self.game.tetris.state == 'gameover'
 		info = None
 
-		reward = self.__calc_reward()
+		reward = self.__calc_reward(figure_before_step, next_figure_before_step)
 		self.last_score = self.game.tetris.score
 
 		if done:
@@ -36,7 +43,23 @@ class TetrisEnv(gym.Env):
 		self.last_score = 0
 		return self.render()
 
-	def __calc_reward(self):
+	def __calc_reward(self, figure_before_step, next_figure_before_step):
 		base_score = ((self.game.tetris.score - self.last_score) * 100000) + 1
 		figure_score = self.game.tetris.figure.y**2
-		return base_score + figure_score
+
+		extra_reward = 0
+		#  new figure
+		if self.game.tetris.next_figure != next_figure_before_step:
+			line_dropped = figure_before_step.y
+			extra_reward += self.__check_lines_for_placement(line_dropped)
+
+		return base_score + figure_score + extra_reward
+
+	def __check_lines_for_placement(self, line_y):
+		base_fill_score = 0
+		for y in range(line_y, min(line_y + 4, 19)):
+			line_count = len(list(filter(lambda x: x > 0, self.game.tetris.field[y])))
+			line_percentage = line_count / 10
+			base_fill_score += line_percentage * y**2
+
+		return base_fill_score
