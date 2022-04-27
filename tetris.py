0b6523ab82f13022711e1cb8be85254c53a25411
Robin Tretter <robin.tretter@gmail.com>
2022-04-27 15:33:45 +0200

    fixed top line

--- tetris.py
+++ tetris.py
@@ -55,7 +55,10 @@ class Tetris:
                 lines += 1
                 for i1 in range(i, 1, -1):
                     for j in range(self.width):
-                        self.field[i1][j] = self.field[i1 - 1][j]
+                        if i1 == 0:
+                            self.field[i1][j] = 0
+                        else:
+                            self.field[i1][j] = self.field[i1 - 1][j]
         self.score += lines ** 2
 
     def go_space(self):
